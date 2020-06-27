5cd2d09281e4a5eb0067f1ce5a1864cdfcaad8cf
Yueh85217 <yueh85217@gmail.com>
2020-06-27 14:51:20 +0800

    fixed pc_control to a simple wire instead of ff register

--- code/pc.v
+++ code/pc.v
@@ -1,29 +1,17 @@
 // PC control unit
 
 
-module pc_control(clk,
-				  rst_n,
-				  PC,
+module pc_control(PC,
 				  PC_nxt,
 				  imm,
 				  branch);
 	
-	input 		    clk, rst_n, branch	;
+	input 		    branch	;
 	input 	[31:0]	PC, imm				;
 	output 	[31:0]	PC_nxt				;
-	reg 	[31:0]	PC_nxt				;
-	reg		[31:0]  IMM					;
+   	wire	[31:0]  IMM					;
 
-	always @(*) begin
-		IMM = imm << 1;
-	end
+	assign IMM = imm << 1;
+	assign PC_nxt = branch?(PC+IMM):(PC+4);
 
-	always @(posedge clk or negedge rst_n) begin
-		if (!rst_n) begin
-			PC_nxt <= 32'h00010000;
-		end
-		else begin
-			PC_nxt <= branch?(PC+IMM):(PC+4);
-		end
-	end
 endmodule
