8cf07cb6037b3171b5fac656e71b48e3930ffa8f
Yueh85217 <yueh85217@gmail.com>
2020-06-27 14:15:40 +0800

    added immediate adder

--- code/pc.v
+++ code/pc.v
@@ -5,20 +5,25 @@ module pc_control(clk,
 				  rst_n,
 				  PC,
 				  PC_nxt,
-				  dest,
+				  imm,
 				  branch);
 	
 	input 		    clk, rst_n, branch	;
-	input 	[31:0]	PC, dest			;
+	input 	[31:0]	PC, imm				;
 	output 	[31:0]	PC_nxt				;
 	reg 	[31:0]	PC_nxt				;
+	reg		[31:0]  IMM					;
+
+	always @(*) begin
+		IMM = imm << 1;
+	end
 
 	always @(posedge clk or negedge rst_n) begin
 		if (!rst_n) begin
 			PC_nxt <= 32'h00010000;
 		end
 		else begin
-			PC_nxt <= branch?dest:PC+4;
+			PC_nxt <= branch?(PC+IMM):(PC+4);
 		end
 	end
-end module
+endmodule
